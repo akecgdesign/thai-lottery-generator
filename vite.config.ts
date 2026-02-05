a15249272d38683a44af0236cf56d797f348dbca
akecgdesign <akecgdesign@gmail.com>
2026-02-05 12:09:49 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,10 @@
-import { defineConfig } from 'vite'; export default defineConfig({ build: { outDir: 'dist' }, server: { port: 3000 } });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
+});
